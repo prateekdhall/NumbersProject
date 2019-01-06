8bb30d298fe8a3ef518a072802417b8cf6173256
prateekdhall@gmail.com <prateekdhall@gmail.com>
2019-01-06 21:08:22 +0530

    [Code Commit]

---
+++ src/test/java/devops/numbers/NumberWordsTests.java
@@ -0,0 +1,22 @@
+package devops.numbers;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import devops.numbers.NumberWords;
+
+public class NumberWordsTests {
+	private NumberWords numberWords ;
+	
+	@Before
+	public void setup() {
+		numberWords = new NumberWords() ;
+	}
+
+	@Test
+	public void numberOutOfRangeReturnsError() {
+		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
+	}
+
+}
